fcce1d6c53db938c3c3756f17b01d967b2ffac6f
Coffee58511867 <marobahlalele910@gmail.com>
2024-07-31 21:21:27 +0200

    Created authors api

--- node_api/urls.py
+++ node_api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from node_api.views import Notes, Authors, NoteDetail, AuthorDetail, Users
 
 urlpatterns = [
-    path('', Notes.as_view()),
+    path('notes/', Notes.as_view()),
     path('notes/<str:pk>/', NoteDetail.as_view(), name='note-detail'),
     path('authors/', Authors.as_view()),
     path('users/', Users.as_view()),
