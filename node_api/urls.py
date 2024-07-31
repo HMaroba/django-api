b50ca4bd816f3e278161dd70367ca49adbed7a10
HMaroba <marobahlalele910@gmail.com>
2024-07-31 17:03:34 +0200

    Initial commit

--- node_api/urls.py
+++ node_api/urls.py
@@ -1,7 +1,17 @@
+# from django.urls import path
+# from node_api.views import Notes, NoteDetail
+
+# urlpatterns = [
+#     path('', Notes.as_view()),
+#     path('<str:pk>', NoteDetail.as_view())
+# ]
+
 from django.urls import path
-from node_api.views import Notes, NoteDetail
+from node_api.views import Notes, NoteDetail, AuthorDetail
 
 urlpatterns = [
-    path('', Notes.as_view()),
-    path('<str:pk>', NoteDetail.as_view())
+    path('', Notes.as_view(), name='notes'),
+    path('notes/<str:pk>/', NoteDetail.as_view(), name='note-detail'),
+    # path('authors/', Authors.as_view(), name='authors'),
+    path('authors/<str:pk>/', AuthorDetail.as_view(), name='author-detail')
 ]
